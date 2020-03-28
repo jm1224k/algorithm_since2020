b7af77dc0204d60ee49db76aa016cfc47b7b62a2
jm1224k <38150578+jm1224k@users.noreply.github.com>
2020-03-28 17:19:40 +0900

    Update jm_seoul2ks.cpp

--- algorithm/programmers/jm_seoul2ks.cpp
+++ algorithm/programmers/jm_seoul2ks.cpp
@@ -9,15 +9,15 @@ int solution(int K, vector<vector<int>> travel) {
 	vector<int> d(100003);
 	
 	for (int i = 0; i < travel.size(); i++)
-    {
+	{
 		for (int j = K; j >= 0; j--)
-        {
+        	{
 			d[j] = INT_MIN;
             
 			if (j >= travel[i][0])
 				d[j] = max(d[j], d[j - travel[i][0]] + travel[i][1]);
 			
-            if (j >= travel[i][2])
+            		if (j >= travel[i][2])
 				d[j] = max(d[j], d[j - travel[i][2]] + travel[i][3]);
 		}
 	}
