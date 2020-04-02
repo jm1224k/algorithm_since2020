55aae30278ef8b1f316a78ee687a296d5a992121
hhaahaha <49986339+hhaahaha@users.noreply.github.com>
2020-04-02 20:05:49 +0900

    Create 이중우선순위큐.py

---
+++ algorithm/programmers/이중우선순위큐.py
@@ -0,0 +1,34 @@
+import heapq
+
+def solution(operations):
+    cnt = 0
+    minhq = []
+    maxhq = []
+
+    for operation in operations:
+
+        if operation[0] == 'I':
+            number = int(operation[2:])
+            heapq.heappush(minhq,number)
+            heapq.heappush(maxhq,-number)
+            cnt += 1
+
+        else:
+            if cnt != 0:
+                if operation == 'D 1':
+                    heapq.heappop(maxhq)
+
+                elif operation == 'D -1':
+                    heapq.heappop(minhq)
+
+                cnt -= 1
+            if cnt == 0:
+                maxhq = []
+                minhq = []
+
+    if cnt != 0:
+        answer = [-heapq.heappop(maxhq),heapq.heappop(minhq)]
+    else:
+        answer = [0,0]
+
+    return answer
